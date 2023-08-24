b8139e59ac2bfb953e2386af7c18d08cb54c0219
Alexandr Dikarev <enotov21@yandex.ru>
2023-08-24 22:20:00 +0300

    Test Deploy3

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 			<a href="" class="m">Новости</a>
 			<a href="" class="m">О нас</a>
 			<a href="" class="m">Контакты</a>
-			<a href="" class="m">Jobs and ll22</a>
+			<a href="" class="m">Jobs and ll222</a>
 		</nav>
 	</div>
 		
