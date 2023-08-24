cd873d5617fa2c538116e95c658ac1dd3c0fc5f3
AlexEnotov <116025781+AlexEnotov@users.noreply.github.com>
2023-08-24 15:25:05 +0300

    Up v1
    Create index, style
    
    Add Menu
    Add style

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php header("Refresh:10");?>
+	
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<title>Portal</title>
+</head>
+<body>
+	<div class="menu">
+		<nav>
+			<div class="logo">Портал</div>
+			<a href="" class="m">Главная</a>
+			<a href="" class="m">Новости</a>
+			<a href="" class="m">О нас</a>
+			<a href="" class="m">Контакты</a>
+		</nav>
+	</div>
+		
+
+
+</body>
+</html>
