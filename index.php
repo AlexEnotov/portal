f818d7cdbbb409b83717c2ab4ed062ce83b2e58d
AlexEnotov <116025781+AlexEnotov@users.noreply.github.com>
2023-08-25 21:43:50 +0300

    New d2

--- index.php
+++ index.php
@@ -6,19 +6,20 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="style.css">
+	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 	<title>Portal</title>
 </head>
 <body>
 	<div class="menu">
 		<nav>
-			<div class="logo">Портал ртк-цд</div>
-			<a href="" class="m">Главная</a>
-			<a href="" class="m">Новости</a>
-			<a href="" class="m">О нас</a>
-			<a href="" class="m">Контакты</a>
-			<a href="" class="m">Jobs</a>
+			<div class="logo"></div>
+			<a href="" class="m"><i class='bx bxs-dashboard' ></i> Главная</a>
+			<a href="" class="m"><i class='bx bx-store-alt' ></i> Новости</a>
+			<a href="" class="m"><i class='bx bx-analyse' ></i> О нас</a>
+			<a href="" class="m"><i class='bx bx-message-square-dots' ></i> Контакты</a>
+			<a href="" class="m"><i class='bx bx-group' ></i> Jobs</a>
 		</nav>
-	</div>
+	</div></i>
 
 	<div class="menu2">
 		<nav>
