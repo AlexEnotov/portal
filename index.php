5fe865a13612365b3ba34e365954d94855b866aa
AlexEnotov <116025781+AlexEnotov@users.noreply.github.com>
2023-08-25 17:47:01 +0300

    Update

--- index.php
+++ index.php
@@ -11,7 +11,18 @@
 <body>
 	<div class="menu">
 		<nav>
-			<div class="logo">Портал</div>
+			<div class="logo">Портал ртк-цд</div>
+			<a href="" class="m">Главная</a>
+			<a href="" class="m">Новости</a>
+			<a href="" class="m">О нас</a>
+			<a href="" class="m">Контакты</a>
+			<a href="" class="m">Jobs</a>
+		</nav>
+	</div>
+
+	<div class="menu2">
+		<nav>
+			<div class="logo">Портал ртк-цд</div>
 			<a href="" class="m">Главная</a>
 			<a href="" class="m">Новости</a>
 			<a href="" class="m">О нас</a>
