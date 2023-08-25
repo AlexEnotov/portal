7214c83d1c50da10ceaebd99b328dc24ccf7136e
AlexEnotov <116025781+AlexEnotov@users.noreply.github.com>
2023-08-25 15:40:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 			<a href="" class="m">Новости</a>
 			<a href="" class="m">О нас</a>
 			<a href="" class="m">Контакты</a>
-			<a href="" class="m">Jobs222</a>
+			<a href="" class="m">Jobs</a>
 		</nav>
 	</div>
 		
