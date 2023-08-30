a8d49384b0ffd76add9e6067786750ed678e99e4
AlexEnotov <116025781+AlexEnotov@users.noreply.github.com>
2023-08-30 14:51:27 +0300

    fixed news button and news pages

--- index.php
+++ index.php
@@ -10,29 +10,7 @@
 	<title>Portal</title>
 </head>
 <body>
-	<div class="menu">
-		<nav>
-			<div class="logo"></div>
-			<a href="" class="m"><i class='bx bxs-dashboard' ></i> Главная</a>
-			<a href="" class="m"><i class='bx bx-store-alt' ></i> Новости</a>
-			<a href="" class="m"><i class='bx bx-analyse' ></i> О нас</a>
-			<a href="" class="m"><i class='bx bx-message-square-dots' ></i> Контакты</a>
-			<a href="" class="m"><i class='bx bx-group' ></i> Jobs</a>
-			<a href="" class="m e"><i class='bx bx-exit'></i> Exit</a>
-		</nav>
-	</div></i>
-
-	<div class="menu2">
-		<nav>
-			<div class="logo">Портал ртк-цд</div>
-			<a href="" class="m">Главная</a>
-			<a href="" class="m">Новости</a>
-			<a href="" class="m">О нас</a>
-			<a href="" class="m">Контакты</a>
-			<a href="" class="m">Jobs</a>
-
-		</nav>
-	</div>
+	<?php include 'layout/menu.php'; ?>
 <div class="main-content">
 	<div class="news-one">
 		<div class="head-banner"><img src="banner.jpg" alt=""></div>
@@ -45,6 +23,10 @@
 			<div class="content">
 				<p>Например какой-то текст для тестирования новостного блока, для того чтобы понять как его выстроить и зачем он нужен, а так же понять какой должен быть внешний вид у всех трех блоков, содержащих ТОП 3 последних новости из Head-ленты для главной страницы, которые будут первыми открываться при посещении сайта.</p>
 			</div>
+			<div class="footer">
+				<hr style="margin:0 5px 0 5px;">
+				<h4><a href="#"><i class='bx bxs-chevron-right' ></i> Читать..</a></h4>
+			</div>
 		</div>
 		<div class="block">
 			<div class="header-c">
@@ -52,6 +34,10 @@
 			</div>
 			<div class="content">
 				<p>Например какой-то текст для тестирования новостного блока, для того чтобы понять как его выстроить и зачем он нужен, а так же понять какой должен быть внешний вид у всех трех блоков, содержащих ТОП 3 последних новости из Head-ленты для главной страницы, которые будут первыми открываться при посещении сайта.</p>
+			</div>
+			<div class="footer">
+				<hr style="margin:0 5px 0 5px;">
+				<h4><a href="#"><i class='bx bxs-chevron-right' ></i> Читать..</a></h4>
 			</div>
 		</div>
 		<div class="block">
@@ -61,6 +47,11 @@
 			<div class="content">
 				<p>Например какой-то текст для тестирования новостного блока, для того чтобы понять как его выстроить и зачем он нужен, а так же понять какой должен быть внешний вид у всех трех блоков, содержащих ТОП 3 последних новости из Head-ленты для главной страницы, которые будут первыми открываться при посещении сайта. Это уже третий блок для проверки, последняя запись из ТОП 3 актуальных записей данного новостного блока</p>
 			</div>
+			<div class="footer">
+				<hr style="margin:0 5px 0 5px;">
+				<h4><a href="#"><i class='bx bxs-chevron-right' ></i> Читать..</a></h4>
+			</div>
+
 		</div>
 	</div>
 </div>		
---
+++ layout/menu.php
@@ -0,0 +1,25 @@
+<div class="menu">
+		<nav>
+			<div class="logo"></div>
+			<a href="" class="m"><i class='bx bxs-dashboard' ></i> Главная</a>
+			<a href="" class="m"><i class='bx bx-store-alt' ></i> Новости</a>
+			<a href="" class="m"><i class='bx bx-analyse' ></i> О нас</a>
+			<a href="" class="m"><i class='bx bx-message-square-dots' ></i> Контакты</a>
+			<a href="" class="m"><i class='bx bx-group' ></i> Jobs</a>
+			<a href="views/admin.php" class="m"><i class='bx bx-shield-quarter' style='color:var(--primary);'></i> Админка</a>
+			<a href="" class="m e"><i class='bx bx-exit'></i> Exit</a>
+		</nav>
+	</div></i>
+
+	<div class="menu2">
+		<nav>
+			<div class="logo">Портал ртк-цд</div>
+			<a href="" class="m">Главная</a>
+			<a href="" class="m">Новости</a>
+			<a href="" class="m">О нас</a>
+			<a href="" class="m">Контакты</a>
+			<a href="" class="m">Jobs</a>
+
+
+		</nav>
+	</div>
