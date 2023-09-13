f2a70fc7c7ebdac27d7da90592b30dbcfa420d67
AlexEnotov <116025781+AlexEnotov@users.noreply.github.com>
2023-09-13 14:59:20 +0300

    up new project

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-
-	
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,58 +5,82 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="style.css">
 	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-	<title>Portal</title>
+	<title>Execut</title>
 </head>
 <body>
 	<?php include 'layout/menu.php'; ?>
-<div class="main-content">
-	<div class="news-one">
-		<div class="head-banner"><img src="banner.jpg" alt=""></div>
-	</div>
-	<div class="blocks">
-		<div class="block">
-			<div class="header-c">
-				<h2>Какое-то название</h2>
-			</div>
-			<div class="content">
-				<p>Например какой-то текст для тестирования новостного блока, для того чтобы понять как его выстроить и зачем он нужен, а так же понять какой должен быть внешний вид у всех трех блоков, содержащих ТОП 3 последних новости из Head-ленты для главной страницы, которые будут первыми открываться при посещении сайта.</p>
-			</div>
-			<div class="footer">
-				<hr style="margin:0 5px 0 5px;">
-				<h4><a href="#"><i class='bx bxs-chevron-right' ></i> Читать..</a></h4>
-			</div>
+	<div class="content">
+		<div class="banner">
+			<div style="text-align: center;padding: 95px 0 50px 0;font-size: 50px;">КАКОЙ ТО БАННЕР</div>
 		</div>
-		<div class="block">
-			<div class="header-c">
-				<h2>Какое-то название</h2>
-			</div>
-			<div class="content">
-				<p>Например какой-то текст для тестирования новостного блока, для того чтобы понять как его выстроить и зачем он нужен, а так же понять какой должен быть внешний вид у всех трех блоков, содержащих ТОП 3 последних новости из Head-ленты для главной страницы, которые будут первыми открываться при посещении сайта.</p>
-			</div>
-			<div class="footer">
-				<hr style="margin:0 5px 0 5px;">
-				<h4><a href="#"><i class='bx bxs-chevron-right' ></i> Читать..</a></h4>
-			</div>
+		<div class="block_news">
+			<form action="" style="padding: 20px; width: 100%;height: 100%;">
+				<div class='btn-group'>
+					<button class="btn1"><b>НОВОСТЬ</b></button>
+					<button class="btn2"><b>ОПРОС</b></button>
+					<button class="btn2"><b>ОБЪЯВЛЕНИЕ</b></button>
+					<button class="btn3"><b>СОБЫТИЕ</b></button>
+				</div>
+				<textarea class="ta active" name="qwe" id="q" cols="103%" rows="5%" placeholder="Тут какой-то текст..."></textarea>
+				<button class="btn"><b>ОТПРАВИТЬ</b></button>
+			</form>
 		</div>
-		<div class="block">
-			<div class="header-c">
-				<h2>Какое-то название</h2>
+		<div class="min-block">
+			<div class="min-block-c1">
+				<div class="title"><b>Опросы</b></div>
+				<div class="b-content">
+					<img src="https://i.pinimg.com/originals/97/49/7d/97497dc64fc934b2d7f8571e820e1b9c.png" alt="" style="width: 220px; "><br>
+					<br><p>Опрос на тему жизни</p><br>
+					<button class="btn-c"><b>ПЕРЕЙТИ К ОПРОСАМ</b></button>
+				</div>
 			</div>
-			<div class="content">
-				<p>Например какой-то текст для тестирования новостного блока, для того чтобы понять как его выстроить и зачем он нужен, а так же понять какой должен быть внешний вид у всех трех блоков, содержащих ТОП 3 последних новости из Head-ленты для главной страницы, которые будут первыми открываться при посещении сайта. Это уже третий блок для проверки, последняя запись из ТОП 3 актуальных записей данного новостного блока</p>
+			<div class="min-block-c2">
+				<div class="title"><b>Сегодня день рождения! <i class='bx bx-happy-alt bx-sm' style="color: var(--warning);"></i></b></div>
+				<div class="b-content">
+					<table>
+						<tr>
+							<td>1.</td>
+							<td>Иванов Иван Иванович</td>
+						</tr>
+						<tr>
+							<td>2.</td>
+							<td>Алексеев Алексей Алексеевич</td>
+						</tr>
+					</table>
+				</div>
 			</div>
-			<div class="footer">
-				<hr style="margin:0 5px 0 5px;">
-				<h4><a href="#"><i class='bx bxs-chevron-right' ></i> Читать..</a></h4>
-			</div>
-
+			
 		</div>
-	</div>
-	<div class="other-blocks">
-		
-	</div>
-</div>		
 
+		<div class="block_news2">
+			<div class="header">Актуальные новости</div>
+			<div class="new_n">
+				<div class="title">Внимание коллеги! Корпоративное мероприятие 24 сентября!</div>
+				<div class="created">13.09.2023</div>
+				<div class="content_n">24 сентября будет проводиться корпоративное мероприятие, посвященное знакомству коллег из разных офисов и городов!</div>
+				<div class="actions" style="width: 100%;"><a href="#" class="btn_n"><b>Читать..</b></a></div>
+				<hr>
+			</div>
+			<div class="new_n">
+				<div class="title">Открытие нового корпоративного портала!</div>
+				<div class="created">12.09.2023</div>
+				<div class="content_n">Сегодня у нашего дружного коллектива появилось своё уютное местечко! Теперь все новинки в компании будут на нашем новом портале!</div>
+				<div class="actions" style="width: 100%;"><a href="#" class="btn_n"><b>Читать..</b></a></div>
+				<hr>
+			</div>
+			<div class="new_n">
+				<div class="title">Тестирование портала.</div>
+				<div class="created">11.09.2023</div>
+				<div class="content_n">Тестирование портала идет полным ходом! Осталось совсем чуть-чуть!</div>
+				<div class="actions" style="width: 100%;"><a href="#" class="btn_n"><b>Читать..</b></a></div>
+				<hr>
+			</div>
+			
+		</div>
 
+	</div>
 </body>
 </html>
+
+<!-- <i class='bx bx-chevron-right' ></i>
+<i class='bx bx-chevron-down' ></i> -->
--- layout/menu.php
+++ layout/menu.php
@@ -1,28 +1,90 @@
-<div class="bar"><i class='bx bx-menu' ></i></div>
 <div class="menu">
 
 		<nav>
 
 			<div class="logo"></div>
-			<a href="/" class="m"><i class='bx bxs-dashboard' ></i> Главная</a>
-			<a href="/views/news.php" class="m"><i class='bx bx-store-alt' ></i> Новости</a>
-			<a href="" class="m"><i class='bx bx-analyse' ></i> О нас</a>
-			<a href="" class="m"><i class='bx bx-message-square-dots' ></i> Контакты</a>
-			<a href="" class="m"><i class='bx bx-group' ></i> Jobs</a>
-			<a href="/views/admin.php" class="m"><i class='bx bx-shield-quarter' style='color:var(--primary);'></i> Админка</a>
-			<a href="" class="m e"><i class='bx bx-exit'></i> Exit</a>
+			<a href="/" class="m"><i class='bx bx-home' ></i>Домашнаяя страница</a>
+			<div class="m"> 
+				<div class="title" onclick="openbox('c1');openboximg('i1');"><i class='bx bx-food-menu'></i><p>Про нас</p><i class='bx bx-chevron-left s' id='i1'></i></div>
+				<div class="child" id="c1" style="display: none; height: 0%;">
+					<a href="/views/about/наш_проект.php"><div class="item">Наш проект</div></a>
+					<a href="/views/about/о_компании.php"><div class="item">О Компании</div></a>
+					<a href=""><div class="item">Лидеры Компании</div></a>
+					<a href=""><div class="item">Сотрудники</div></a>
+					<a href="/views/about/миссия_компании.php"><div class="item">Миссия Компании</div></a>
+				</div>
+			</div>
+			<div class="m"> 
+				<div class="title" onclick="openbox('c2');openboximg('i2');"><i class='bx bx-news' ></i><p>Новости</p><i class='bx bx-chevron-left s' id='i2'></i></div>
+				<div class="child" id="c2" style="display: none; height: 0%;">
+					<a href=""><div class="item">Новости и достижения</div></a>
+					<a href=""><div class="item">Поздравления</div></a>
+					<a href=""><div class="item">Опросы и конкурсы</div></a>
+					<a href=""><div class="item">Пятничные мемы</div></a>
+				</div>
+			</div>
+			<div class="m"> 
+				<div class="title" onclick="openbox('c3');openboximg('i3');"><i class='bx bx-bookmark-alt-plus'></i><p>Для сотрудников</p><i class='bx bx-chevron-left s' id='i3'></i></div>
+				<div class="child" id="c3" style="display: none; height: 0%;">
+					<a href=""><div class="item">Организационная структура</div></a>
+					<a href=""><div class="item">Справочник</div></a>
+					<a href=""><div class="item">Институт наставничества</div></a>
+				</div>
+			</div>
+			<div class="m"> 
+				<div class="title" onclick="openbox('c4');openboximg('i4');"><i class='bx bx-file' ></i><p>Документы</p><i class='bx bx-chevron-left s' id='i4'></i></div>
+				<div class="child" id="c4" style="display: none; height: 0%;">
+					<a href=""><div class="item">Кадровые документы</div></a>
+					<a href=""><div class="item">ДМС</div></a>
+					<a href=""><div class="item">Программа "Знаю профессионала"</div></a>
+				</div>
+			</div>
+			<a href="" class="m"><i class='bx bx-extension' ></i>Мысли и факты</a>
+			<a href="/views/admin.php" class="m"><i class='bx bx-shield-quarter' style='color:var(--primary);'></i>Настройки</a>
+			<a href="" class="m e"><i class='bx bx-exit'></i>Выход</a>
 		</nav>
 	</div></i>
 
 	<div class="menu2">
 		<nav>
 			<div class="logo">Портал ртк-цд</div>
-			<a href="" class="m">Главная</a>
+			<div class="profile" onclick="openbox('c5')">
+				<img src="https://sun6-23.userapi.com/s/v1/ig1/VQtoKSyYexj6lFKV-E2_-X-UsVOASLNcVMnW7MfkGG6CuphcN2EaY-q4PPy6KQlJ6KuALCjA.jpg?size=2002x2002&quality=96&crop=0,0,2002,2002&ava=1" alt=""  style="width: 35px "/> 
+					<div class="title">Дикарев А.А. <i class='bx bx-chevron-left s'></i></div>
+			</div>
+			<div class="child" id="c5" style="display: none;padding: 5px;  position: fixed;right: 0;top:50px; width: 220px; background: var(--white); border: 1px solid lightgrey;">
+					<a href="#"><div class="item">Профиль</div></a>
+					<a href="#"><div class="item">Записи</div></a>
+					<a href="#"><div class="item">Выйти</div></a>
+				</div>
+			<!-- <a href="" class="m">Главная</a>
 			<a href="" class="m">Новости</a>
 			<a href="" class="m">О нас</a>
 			<a href="" class="m">Контакты</a>
-			<a href="" class="m">Jobs</a>
+			<a href="" class="m">Jobs</a> -->
 
 
 		</nav>
 	</div>
+
+
+	<script>
+function openbox(id){
+    display = document.getElementById(id).style.display;
+    if(display=='none'){
+       document.getElementById(id).style.display='block';
+    }else{
+       document.getElementById(id).style.display='none';
+    }
+};
+function openboximg(id){
+    classs = document.getElementById(id).className;
+
+    if(classs == 'bx bx-chevron-left s'){
+       	document.getElementById(id).className = 'bx bx-chevron-down s';
+    }else{
+		document.getElementById(id).className = 'bx bx-chevron-left s';
+    }
+}
+
+	</script>
